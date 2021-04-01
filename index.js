c99c8c597f4a69f4c28259958ecb598686be9f7d
Lizzette <Lizzetter00@gmail.com>
2021-04-01 15:20:37 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function introduction(name=lizzette){
+    console.log("Hi, my name is lizzette");
+}
+function introductionWithLanguage(name=lizzette, language=javascript){
+    console.log("Hi my name is lizzette and I am learning to program in javascript");
+}
